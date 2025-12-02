e12095409a51b580e66062d708409199f0ebdd7a
Inant Kharisma <inant.kharisma@gmail.com>
2025-12-02 22:28:00 +0700

    add soft deletes

--- app/Models/StockReceipt.php
+++ app/Models/StockReceipt.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class StockReceipt extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $table = 'stock_receipts';
 
--- app/Models/StockReceiptItem.php
+++ app/Models/StockReceiptItem.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class StockReceiptItem extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $table = 'stock_receipt_items';
 
