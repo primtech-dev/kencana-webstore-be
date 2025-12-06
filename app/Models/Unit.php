7f7212550d46bdd6010ec1a6debba5e8d2b5895d
Inant Kharisma <inant.kharisma@gmail.com>
2025-12-06 20:10:55 +0700

    add: migration add units table, add columns in orders table, add unit_id in products and product_variants

---
+++ app/Models/Unit.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Unit extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'units';
+
+    protected $fillable = [
+        'code',
+        'name',
+        'symbol'
+    ];
+
+    // ---- RELATIONS ----
+
+    // Produk yang menggunakan unit ini (base unit)
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'base_unit_id');
+    }
+
+    // Varian produk yang menggunakan unit ini
+    public function productVariants()
+    {
+        return $this->hasMany(ProductVariant::class, 'unit_id');
+    }
+
+    // Order items yang menyimpan snapshot unit ini
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class, 'unit_id');
+    }
+}
